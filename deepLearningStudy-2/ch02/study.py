6a04c386634a563be62e2e5ca918737991f7db41
hppRC <hpp.ricecake@gmail.com>
2019-04-01 00:10:42 +0900

    break

--- deepLearningStudy-2/ch02/study.py
+++ deepLearningStudy-2/ch02/study.py
@@ -1,3 +1,7 @@
+import sys
+sys.path.append('..')
+import numpy as np
+
 def preprocess(text) :
     text = text.lower()
     text = text.replace(".", " .")
@@ -15,4 +19,36 @@ def preprocess(text) :
     corpus = np.array([word_to_id[w] for w in words])
 
     return corpus, word_to_id, id_to_word
+
+def create_to_matrix(corpus, vocab_size, window_size=1) :
+    corpus_size = len(corpus)
+    co_matrix = np.zeros((vocab_size, vocab_size), dtype=np.int32)
+
+    for idx, word_id in enumerate(corpus) :
+        for i in range(1, window_size+1) :
+            left_idx = idx - i
+            right_idx = idx + i
+
+            if (left_idx >= 0) :
+                left_word_id = corpus[left_idx]
+                co_matrix[word_id, left_word_id] += 1
+            
+            if (right_idx < corpus_size) :
+                right_word_id = corpus[right_idx]
+                co_matrix[word_id, right_word_id] += 1
+
+    return co_matrix
+
+def cos_similarity(x, y) :
+    nx /= np.sqrt(np.sum(x**2))
+    ny /= np.sqrt(np.sum(y**2))
+    
+    return np.dot(nx, ny)
+
+
+text = "You say goodbye and I say hello."
+
+corpus, word_to_id, id_to_word = preprocess(text)
+
+print(create_to_matrix(corpus, len(word_to_id)))
 
