68174d2d3a11a9eb0d0c3240057a53d560bb7467
hppRC <hpp.ricecake@gmail.com>
2019-04-08 03:27:31 +0900

    it was hard task

--- ReactStudy/ch4/electron_hello/main.js
+++ ReactStudy/ch4/electron_hello/main.js
@@ -1,24 +1,30 @@
+// Electronの実行に必要なモジュールを取り込む
 const electron = require('electron')
 const path = require('path')
 const url = require('url')
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
 
-let mainWindow
+// Electronのライフサイクルを定義
+let mainWindow // メインウィンドウを表す変数
 app.on('ready', createWindow)
 app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') app.quit()
+  if (process.platform !== 'darwin') app.quit()
+})
+app.on('activate', function () {
+  if (mainWindow === null) createWindow()
 })
 
+// ウィンドウを作成してコンテンツを読み込む
 function createWindow () {
-    mainWindow = new BrowserWindow({width:800, height:600})
-    mainWindow.loadURL(url.format({
-       pathname: path.join(__dirname, 'index.html'),
-       protocol: 'file:',
-       slashes: true 
-    }))
-
-    mainWindow.on('closed', function () {
-        mainWindow = null
-    })
+  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow.loadURL(url.format({ // 読み込むコンテンツを指定 --- (※1)
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+  // ウィンドウが閉じるときの処理
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
 }
