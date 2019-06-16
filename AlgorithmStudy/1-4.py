b7fe6b84f1ebd44fb70ae4fb2d92c6d4c1399bec
hppRC <hpp.ricecake@gmail.com>
2019-06-16 13:56:09 +0900

    change function name from isPalindromeByHash to isAsciiPalindoromeByHash

--- AlgorithmStudy/1-4.py
+++ AlgorithmStudy/1-4.py
@@ -1,4 +1,4 @@
-def isPalindoromeByHash(string):
+def isAsciiPalindoromeByHash(string):
     string = "".join(string.split())
     hash = [0]*128
     for ch in string:
@@ -11,7 +11,14 @@ def isPalindoromeByHash(string):
             else:
                 flag = True
     return True
-        
+
+
+def isAsciiPalindoromeByBit(string):
+    string = "".join(string.split())
+    hashBit = 0
+    for ch in string:
+        hashBit ^= 1 << ord(ch)
+    print(bin(hashBit))
 
 if __name__ == "__main__":
-    print(isPalindoromeByHash("hash"))
+    print(isAsciiPalindoromeByBit("hahzz"))
