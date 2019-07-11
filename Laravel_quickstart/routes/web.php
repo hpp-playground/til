4eafb6141de442f8765dfa8557984d563b92a6a2
hppRC <hpp.ricecake@gmail.com>
2019-07-12 01:19:10 +0900

    first commit

---
+++ Laravel_quickstart/routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+use App\Task;
+use Illuminate\Http\Request;
+
+Route::get('/', function () {
+    //
+});
+
+
+Route::post('/task', function (Request $request) {
+    //
+});
+   
+Route::delete('/task/{task}', function (Task $task) {
+    //
+});
+
+
+/*
+
+--default program--
+
+Route::get('/', function () {
+    return view('welcome');
+});
+*/
