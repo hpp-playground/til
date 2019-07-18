215efd9b6d107fd24960b7fcdd55c0b72e3c967c
hppRC <hpp.ricecake@gmail.com>
2019-07-18 20:25:46 +0900

    laravel updated

--- LaravelProject/sampleapp/resources/views/home.blade.php
+++ LaravelProject/sampleapp/resources/views/home.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="ja">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,12 +7,11 @@
     <title>Document</title>
 </head>
 <body>
-    {{ app()->getLocale() }}
-    こんにちは
-
+    こんにちは!
     @if (Auth::check())
         {{ \Auth::user()->name }}さん
     @else
+        ゲストさん<br />
         <a href="/auth/register">会員登録</a>
     @endif
 </body>
