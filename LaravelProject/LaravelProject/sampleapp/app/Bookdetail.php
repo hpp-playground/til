1972c97c6b203c8deac37523fb137810a2756926
hppRC <hpp.ricecake@gmail.com>
2019-07-19 14:51:36 +0900

    add eloquent files

--- LaravelProject/LaravelProject/sampleapp/app/Bookdetail.php
+++ LaravelProject/LaravelProject/sampleapp/app/Bookdetail.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Bookdetail extends Model
 {
-    //
+    public function book()
+    {
+        
+        return $this->belongsTo('\App\Book');
+    }
 }
