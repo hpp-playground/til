7b0fbbe157f7ac2680ef9ca5a3b91811a96efce9
hppRC <hpp.ricecake@gmail.com>
2019-07-21 08:21:57 +0900

    renewal nextstep

--- LaravelProject/LaravelProject/nextstep/app/Providers/AppServiceProvider.php
+++ LaravelProject/LaravelProject/nextstep/app/Providers/AppServiceProvider.php
@@ -4,29 +4,24 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
-use App\DataProvider\Database\RegisterReviewDataProvider;
-use App\DataProvider\RegisterReviewProviderInterface;
-
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        $this->app->bind(RegisterReviewProviderInterface::class, function () {
-            return new RegisterReviewDataProvider();
-        });
-    }
-
     /**
      * Bootstrap any application services.
      *
      * @return void
      */
     public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
     {
         //
     }
--- LaravelProject/LaravelProject/nextstep/app/Providers/EventServiceProvider.php
+++ LaravelProject/LaravelProject/nextstep/app/Providers/EventServiceProvider.php
@@ -3,12 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Event;
-use Illuminate\Auth\Events\Registered;
-use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-
-use App\Events\ReviewRegistered;
-use App\Listeners\ReviewIndexCreator;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -18,13 +13,9 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
+        'App\Events\Event' => [
+            'App\Listeners\EventListener',
         ],
-
-        ReviewRegistered::class => [
-            ReviewIndexCreator::class,
-        ]
     ];
 
     /**
