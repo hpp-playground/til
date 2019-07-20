1ddb88a8c9a051503133e9d8bac28185041513e4
hppRC <hpp.ricecake@gmail.com>
2019-07-21 08:21:57 +0900

    renewal nextstep

--- LaravelProject/nextstep/database/seeds/DatabaseSeeder.php
+++ LaravelProject/nextstep/database/seeds/DatabaseSeeder.php
@@ -1,27 +1,16 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use Illuminate\Database\Schema\Blueprint;
 
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        Schema::table('reviews', function (Blueprint $table) {
-            $table->foreign('user_id')->references('id')
-                ->on('users')->onDelete('cascade')->onUpdate('cascade');
-        });
-
-        Schema::table('review_tags', function (Blueprint $table) {
-            $table->foreign('review_id')->references('id')
-                ->on('reviews')->onDelete('cascade')->onUpdate('cascade');
-            $table->foreign('tag_id')->references('id')
-                ->on('tags')->onDelete('cascade')->onUpdate('cascade');
-        });
+        // $this->call(UsersTableSeeder::class);
     }
 }
