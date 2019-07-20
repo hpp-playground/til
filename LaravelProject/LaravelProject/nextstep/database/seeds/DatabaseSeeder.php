b83f3f20b6c1871e3a2a6e3e45489c1328222fd9
hppRC <hpp.ricecake@gmail.com>
2019-07-20 18:47:12 +0900

    add register reviewing function

--- LaravelProject/LaravelProject/nextstep/database/seeds/DatabaseSeeder.php
+++ LaravelProject/LaravelProject/nextstep/database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Schema\Blueprint;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        Schema::table('reviews', function (Blueprint $table) {
+            $table->foreign('user_id')->references('id')
+                ->on('users')->onDelete('cascade')->onUpdate('cascade');
+        });
+
+        Schema::table('review_tags', function (Blueprint $table) {
+            $table->foreign('review_id')->references('id')
+                ->on('reviews')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('tag_id')->references('id')
+                ->on('tags')->onDelete('cascade')->onUpdate('cascade');
+        });
     }
 }
