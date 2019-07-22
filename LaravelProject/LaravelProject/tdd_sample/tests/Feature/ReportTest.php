8eceb7fde19cd94af0bc03f28453b9ff808eb18c
hppRC <hpp.ricecake@gmail.com>
2019-07-22 14:11:45 +0900

    add all todo funtion

--- LaravelProject/LaravelProject/tdd_sample/tests/Feature/ReportTest.php
+++ LaravelProject/LaravelProject/tdd_sample/tests/Feature/ReportTest.php
@@ -11,20 +11,11 @@ class ReportTest extends TestCase
     /**
      * @test
      */
-    public function testExample()
-    {
-        $this->assertTrue(true);
-    }
-
-    /**
-     * @test
-     */
     public function canAccessApiCustomersByGET()
     {
         $response = $this->get('api/customers');
         $response->assertStatus(200);
     }
-
 
     /**
      * @test
@@ -35,8 +26,78 @@ class ReportTest extends TestCase
         $response->assertStatus(200);
     }
 
+    /**
+     * @test
+     */
     public function canAccessApiCustomersCustomer_idByGET()
     {
+        $response = $this->get('api/customers/1');
         $response->assertStatus(200);
     }
+
+
+    /**
+     * @test
+     */
+    public function canAccessApiCustomersCustomer_idByPUT()
+    {
+        $response = $this->post('api/customers/1');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function canAccessApiCustomersCustomer_idByDELETE()
+    {
+        $response = $this->delete('api/customers/1');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function canAccessApiReportsByGET()
+    {
+        $response = $this->get('api/reports');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function canAccessApiReportsByPOST()
+    {
+        $response = $this->post('api/reports');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function canAccessApiReportsReport_idByGET()
+    {
+        $response = $this->get('api/reports/1');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function canAccessApiReportsReport_idByPOST()
+    {
+        $response = $this->post('api/reports/1');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function canAccessApiReportsReport_idByDELETE()
+    {
+        $response = $this->delete('api/reports/1');
+        $response->assertStatus(200);
+    }
+
+
 }
