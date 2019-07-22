1a9ab99ed789d6d80f4ea89b4d8fd2059c762f9d
hppRC <hpp.ricecake@gmail.com>
2019-07-22 13:04:38 +0900

    add get(api/customers) and post(api/customers)

--- LaravelProject/LaravelProject/tdd_sample/tests/Feature/ReportTest.php
+++ LaravelProject/LaravelProject/tdd_sample/tests/Feature/ReportTest.php
@@ -9,7 +9,6 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class ReportTest extends TestCase
 {
     /**
-     *
      * @test
      */
     public function testExample()
@@ -18,12 +17,26 @@ class ReportTest extends TestCase
     }
 
     /**
-     *
      * @test
      */
     public function canAccessApiCustomersByGET()
     {
         $response = $this->get('api/customers');
+        $response->assertStatus(200);
+    }
+
+
+    /**
+     * @test
+     */
+    public function canAccessApiCustomersByPOST()
+    {
+        $response = $this->post('api/customers');
+        $response->assertStatus(200);
+    }
+
+    public function canAccessApiCustomersCustomer_idByGET()
+    {
         $response->assertStatus(200);
     }
 }
