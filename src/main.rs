d50d0b3792b3ae249b577c381980ba3e2c9350bb
hppRC <hpp.ricecake@gmail.com>
2019-08-27 05:02:55 +0900

    test some functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{Error, FromRequest, Path}
+use serde_derive::*;
+
+#[derive(Deseerialize)]
+struct HelloPath {
+    name: String,
 }
+
+fn hello_name(to: Path<HelloPath>) -> impl Responder {
+    format!("Hello {}!", &to.name)
+}
+
