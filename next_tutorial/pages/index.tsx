c3098093fd38d7bc19c652f8ec74522360269e4c
hppRC <hpp.ricecake@gmail.com>
2019-09-10 02:49:18 +0900

    add: initial commit and kickstart next.js project

---
+++ next_tutorial/pages/index.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+const Index = () => (
+    <div>
+        <Link href="/about">
+            <a>About Page</a>
+        </Link>
+        <p>Hello Next.js and TypeScript</p>
+    </div>
+)
+
+export default Index
